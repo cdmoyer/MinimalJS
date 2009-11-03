21c4ddcb05c1d7a1f15dc4c6caed2ee6b4c152dc
Chris Moyer <chris@inarow.net>
2009-11-02 20:27:00 -0500

    Original commit

---
+++ js/cookie.js
@@ -0,0 +1,32 @@
+/* Minimal JS -- Cookies -- http://github.com/cdmoyer/minimaljs
+ * Copyright (c) 2009 Chris Moyer (chris@inarow.net)  
+ *
+ * Usage:
+ * mjs.cookie_set(name, value, [expire minutes])
+ * mjs.cookie_get(name); 
+ *
+ * No options or configuration.  It's minimal.  Take, edit, use.
+ *
+ * MIT (http://www.opensource.org/licenses/mit-license.php) licensed.
+ */
+if (typeof mjs == "undefined") { mjs = {}; }
+(function (mjs) {
+ 	mjs.cookie_set = function (name, value, minutes) {
+		var expires = "";
+		if (minutes) {
+			var expy = new Date();
+			expy.setTime(expy.getTime() + (minutes*60*60*1000));
+			expires = "; " + date.toGMTString();
+		}
+		document.cookie = name + "=" + value + expires + "; path=/";
+	};
+
+	mjs.cookie_get = function (name) {
+		var cookies = "" + document.cookie;
+		var indx = cookies.indexOf(name + '=');
+		if (indx == -1 ) { return false; }
+		var end = cookies.indexOf(';', indx);
+		return cookies.substr(indx + name.length + 1, end == -1 ? cookies.length : end);
+	};
+	return mjs;
+})(mjs);
---
+++ readme.markdown
@@ -0,0 +1,13 @@
+MinimalJS
+==========
+
+[http://github.com/cdmoyer/minimaljs](http://github.com/cdmoyer/minimaljs)
+
+JavaScript libraries are awesome.  jQuery, Dojo, MooTools, YUI, et al. all
+have their place.  Somethimes, though, you just need one or two pieces of 
+functionality on a page or project.  And that's what MinimalJS is for.
+
+Usage
+-----
+
+Copy files into your document path, include them in your page, make use of the functions.
